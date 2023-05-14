f94e2e64ead41a30ad481a2e226c1a6b1e9318a5
Ehirim Uchenna W <ehirimwu@gmail.com>
2023-05-14 23:46:14 +0100

    firct commit of files

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const connectDatabase = require('./config/database');
+const dotenv = require('dotenv');
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+// Config
+dotenv.config({path: './config/config.env'})
+
+
+// Connect Database
+connectDatabase();
+
+app.get('/', (req, res) => {
+    console.log('Hello! This is the home page...')
+})
+
+// Routes
+
+
+// const api = process.env.API_URL
+
+// port = process.env.PORT || 3001;
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
